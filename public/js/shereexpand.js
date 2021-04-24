e0f6044402adf624eafc89f6164bc75d49736c9d
Glitch (hempsterville) <none>
2021-04-24 20:13:43 +0000

    🏉📒 Updated with Glitch

---
+++ public/js/shereexpand.js
@@ -0,0 +1,17 @@
+ AFRAME.registerComponent('sphereexpand', {
+    
+   init: function () {
+
+      let homeworldelements = document.querySelectorAll(".homeworld");
+      let sky = document.querySelector("#sky");
+
+      let sphereloader = () => {
+      sky.setAttribute("src", "#bordeauxtheater");
+      homeworldelements.forEach((homeworldelement) => {
+      homeworldelement.setAttribute("visible", false)})
+      }
+
+      this.el.addEventListener('click', sphereloader);
+        
+   }});
+  
